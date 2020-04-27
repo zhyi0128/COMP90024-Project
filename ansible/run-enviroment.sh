a27427d512d26bc219f1377eb1e7d67279604f80
Yuming Lin <yumingl@student.unimelb.edu.au>
2020-04-28 02:09:43 +1000

    4.28

--- ansible/run-enviroment.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-. ./unimelb-comp90024-2020-grp-38-openrc; ansible-playbook -i hosts --ask-become-pass enviroment.yaml
--- ansible/run-nectar.sh
+++ ansible/run-nectar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2020-grp-38-openrc; ansible-playbook -i hosts --ask-become-pass nectar.yaml
+. ./unimelb-comp90024-2020-grp-38-openrc.sh; ansible-playbook -i inventory/hosts --ask-become-pass nectar.yaml
