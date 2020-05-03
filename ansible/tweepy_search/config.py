929127ab038c29a4fe5d7bac6ad8315849980552
Yuming Lin <yumingl@student.unimelb.edu.au>
2020-05-04 00:38:13 +1000

    ansible with crawler

---
+++ ansible/tweepy_search/config.py
@@ -0,0 +1,58 @@
+app_keys_tokens = [
+{'consumer_key' : 'CP20F8yCMC85K26XY07w4XElp',
+'consumer_secret' : '4t1r4cdlBPGVzkosnZ2gvBqXbet5MbuJIlkuN0JKYufWIdo4yM',
+'access_token' : '1121041186305630208-hG4Jv9cfPOufx3vAgPpBUCODlWsHQH',
+'access_token_secret' : 'OJSXpMxZDzY9XUo2gqoqZcLUyGY1C9duopI4032fywDPb'}
+,
+{'consumer_key' : '2BjmB9QN2UwT7BWGEYJc6mzyQ',
+'consumer_secret' : 'dkP4itLYIM0rqhHef4BiRkEgp8n2STc5CZuddYzjpnRzN3QX0m',
+'access_token' : '1121041186305630208-9pyRCJS3ltExpoKeTqKVrYcdSNnqHg',
+'access_token_secret' : 'dWIS8xzpbuB1T77UZSQCHJGBOX2uT7A82UmiwpyuSfrkq'}
+,
+{'consumer_key' : 'W225IVMaLWc3Cio8Y2ZwHmwXT',
+'consumer_secret' : 'D0Gebz3e1xqrSKKCNbQPCwLsjNdQVZxHguLekTU4zCavWysswy',
+'access_token' : '1121041186305630208-vVcpClv576aYx9OJjVaWJkYA89m7eI',
+'access_token_secret' : 'ZjUk3ppAaudL4KR3oDQo3K6lDMZRKrnGvj2wYRpzfx1uP'}
+,
+{'consumer_key' : 'ahKRXTnEizWqy4oHC4uBFxWuu',
+'consumer_secret' : 'xF2Pc3JwGtSij9Ig0UhW5A5o4RVk1kxcbTk6jMGM7W7XfOub8w',
+'access_token' : '1121041186305630208-85TVCtBvNc3RjW9RjmcBdwJn5FKxQm',
+'access_token_secret' : 'l3qRsugZsCt1MApDSjtCwMFS19Jms2Y2QiGpUPfzeWVit'}
+,
+{'consumer_key' : 'IaRuxafuggm6eZvXdmurzA2MV',
+'consumer_secret' : '9DwkZsuQHFHtnFD5JVfttZ6uNkqsm99yNxvpmPMgSlPxAtqjJ9',
+'access_token' : '1121041186305630208-UJOUSKINRytWJY9OiCF9ANeqNacdMY',
+'access_token_secret' : 'kQjU0Vc4x7oPnSeuOM4Jrs5waE21dtYOKHN9Wi0hcikxB'}
+,
+{'consumer_key' : 'W2pCfanNy4x8YiEEuC1pHbPU8',
+'consumer_secret' : '3XfspXNraGydG3xKYfxZ6wLVaJsCZqYeSx0cOUFSv2ABNHNi8a',
+'access_token' : '1121041186305630208-iHTAQCyxr0QcCPjEH8gpNYTZyomfjQ',
+'access_token_secret' : 'nsNp6LLbb1SacGUbKlUPBFBEiV937Lct9LxHIgB5MYKnL'}
+,
+{'consumer_key' : 'kpC53YdFfY1Q4vXIEhLKM4lhT',
+'consumer_secret' : '3qYR9E9oucDhQSwilPQxUVnmdr1tJwAOi0iMyKCYWy63coEQLZ',
+'access_token' : '1121041186305630208-bFfc3Y4x0ueoHCyCvVUZxeMWRmhloR',
+'access_token_secret' : 'O460teRRz2MJxQRto7ipMz3MyKA2fwZWwW4bMnMVKzKGZ'}
+,
+{'consumer_key' : 'hVmTeuwuAy44Ufr6q6SlIlSST',
+'consumer_secret' : 'BsYDcvgtmbhdGDv57oI5Q3DwSsBEuPtjaIDjALelQsmnQaeQf1',
+'access_token' : '1121041186305630208-9xcHlMmIVGemqa6jNPDYfrXOv05V8l',
+'access_token_secret' : 'NULMzSH41CSFTzJSv8rUu0fPf2VmiqqKX80SqF06LkqT0'}
+]
+
+
+
+search_appid = {
+	'melbourne': 0,
+	'sydney': 1,
+	'brisbane': 2,
+	'perth': 3,
+	'canberra': 4,
+	'hobart': 5,
+	'adelaide': 6,
+	'Darwin': 7
+}
+
+
+
+
