9e5bbeb4d7debf17b5f29cfea099640ac127b3da
Yuming Lin <yumingl@student.unimelb.edu.au>
2020-05-25 19:51:53 +1000

    version1

---
+++ ansible/tweepy/scenario_cities.py
@@ -0,0 +1,190 @@
+import re
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+#rename nltk sentiment module
+sid = SentimentIntensityAnalyzer()
+# #use to find text`s sentiment
+def find_sentiment(text):
+    if text != None:
+        ss = sid.polarity_scores(text)
+        if ss['neu'] == 1:
+            return 0,1,0
+        elif ss['pos'] > ss['neg']:
+            return 1,0,0
+        else:
+            return 0,0,1
+    else:
+        return 0,0,0
+#
+# #used to find text related to china
+def find_china(text):
+    if text != None:
+        text = re.sub(r'[^\w\s]', '', text.lower())
+        china_list = ['china', 'cn']
+        for item in china_list:
+            if item.lower() in text:
+                return 1
+            else:
+                return 0
+
+# #used to find text related to covid_19
+def find_covid_19(text):
+    if text:
+        text = re.sub(r'[^\w\s]', '', text.lower())
+        covid_19_list = ['covid','covid_19','coronavirus','virus','covid19','covid-19','covid 19', 'corona']
+        for item in covid_19_list:
+            if item.lower() in text:
+                return 1
+            else:
+                return 0
+
+def execute_adelaide(id,text):
+    new_dic = {}
+    if text != "None":
+        pos , neu , neg = find_sentiment(text)
+        is_china = find_china(text)
+        is_covid_19 = find_covid_19(text)
+        new_dic = {
+            'tweet_id': id,
+            'suburb':'Greater Adelaide',
+            'text': text,
+            'pos': pos ,
+            'neu': neu ,
+            'neg':neg,
+            'is_china':is_china,
+            'is_covid_19':is_covid_19,
+        }
+    return new_dic
+
+def execute_brisbane(id,text):
+    new_dic = {}
+    if text != "None":
+        pos , neu , neg = find_sentiment(text)
+        is_china = find_china(text)
+        is_covid_19 = find_covid_19(text)
+        new_dic = {
+            'tweet_id': id,
+            'suburb':'Greater Brisbane',
+            'text': text,
+            'pos': pos ,
+            'neu': neu ,
+            'neg':neg,
+            'is_china':is_china,
+            'is_covid_19':is_covid_19,
+        }
+    return new_dic
+
+
+def execute_canberra(id,text):
+    new_dic = {}
+    if text != "None":
+        pos , neu , neg = find_sentiment(text)
+        is_china = find_china(text)
+        is_covid_19 = find_covid_19(text)
+        new_dic = {
+            'tweet_id': id,
+            'suburb':'Australian Capital Territory',
+            'text': text,
+            'pos': pos ,
+            'neu': neu ,
+            'neg':neg,
+            'is_china':is_china,
+            'is_covid_19':is_covid_19,
+        }
+    return new_dic
+
+def execute_darwin(id,text):
+    new_dic = {}
+    if text != "None":
+        pos , neu , neg = find_sentiment(text)
+        is_china = find_china(text)
+        is_covid_19 = find_covid_19(text)
+        new_dic = {
+            'tweet_id': id,
+            'suburb':'Greater Darwin',
+            'text': text,
+            'pos': pos ,
+            'neu': neu ,
+            'neg':neg,
+            'is_china':is_china,
+            'is_covid_19':is_covid_19,
+        }
+    return new_dic
+
+def execute_hobart(id,text):
+    new_dic = {}
+    if text != "None":
+        pos , neu , neg = find_sentiment(text)
+        is_china = find_china(text)
+        is_covid_19 = find_covid_19(text)
+        new_dic = {
+            'tweet_id': id,
+            'suburb':'Greater Hobart',
+            'text': text,
+            'pos': pos ,
+            'neu': neu ,
+            'neg':neg,
+            'is_china':is_china,
+            'is_covid_19':is_covid_19,
+        }
+    return new_dic
+
+def execute_melbourne(id,text):
+    new_dic = {}
+    if text != "None":
+        pos , neu , neg = find_sentiment(text)
+        is_china = find_china(text)
+        is_covid_19 = find_covid_19(text)
+        new_dic = {
+            'tweet_id': id,
+            'suburb':'Greater Melbourne',
+            'text': text,
+            'pos': pos ,
+            'neu': neu ,
+            'neg':neg,
+            'is_china':is_china,
+            'is_covid_19':is_covid_19,
+        }
+    return new_dic
+
+
+def execute_perth(id,text):
+    new_dic = {}
+    if text != "None":
+        pos , neu , neg = find_sentiment(text)
+        is_china = find_china(text)
+        is_covid_19 = find_covid_19(text)
+        new_dic = {
+            'tweet_id': id,
+            'suburb':'Greater Perth',
+            'text': text,
+            'pos': pos ,
+            'neu': neu ,
+            'neg':neg,
+            'is_china':is_china,
+            'is_covid_19':is_covid_19,
+        }
+    return new_dic
+
+def execute_sydney(id,text):
+    new_dic = {}
+    if text != "None":
+        pos , neu , neg = find_sentiment(text)
+        is_china = find_china(text)
+        is_covid_19 = find_covid_19(text)
+        new_dic = {
+            'tweet_id': id,
+            'suburb':'Greater Sydney',
+            'text': text,
+            'pos': pos ,
+            'neu': neu ,
+            'neg':neg,
+            'is_china':is_china,
+            'is_covid_19':is_covid_19,
+        }
+    return new_dic
+
+
+
---
+++ ansible/tweepy/tweepy_search_brisbane.py
@@ -0,0 +1,130 @@
+import tweepy
+import tweepy_tokens
+import time
+import datetime
+import couchdb
+import ssl
+import scenario_cities
+
+start = time.time()
+ssl._create_default_https_context = ssl._create_unverified_context
+IP_ADDRESS = '172.26.134.54'
+USERNAME = 'admin'
+PASSWORD = 'happy'
+ORIGINAL_DATABASE_NAME = 'may_18th_brisbane'
+SCENARIO_DATABASE_NAME = 'scenario_city'
+
+
+def tweepy_tokens(greater_citys_id):
+    consumer_key = tweepy_tokens.all_keys_and_tokens[greater_citys_id]['consumer_key']
+    consumer_secret = tweepy_tokens.all_keys_and_tokens[greater_citys_id]['consumer_secret']
+    access_token = tweepy_tokens.all_keys_and_tokens[greater_citys_id]['access_token']
+    access_token_secret = tweepy_tokens.all_keys_and_tokens[greater_citys_id]['access_token_secret']
+    auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+    return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+
+def create_couchdb():
+    server = couchdb.Server("http://%s:%s@%s:5984/" %
+                            (USERNAME, PASSWORD, IP_ADDRESS))
+    try:
+        original_database = server.create(ORIGINAL_DATABASE_NAME)
+        scenario_database = server.create(SCENARIO_DATABASE_NAME)
+    except:
+        original_database = server[ORIGINAL_DATABASE_NAME]
+        scenario_database = server[SCENARIO_DATABASE_NAME]
+    return original_database, scenario_database
+
+
+def process_tweets(raw_tweets, no_geo_count, no_coordinates, no_place):
+    tweet = raw_tweets.next()._json
+    if tweet['is_quote_status'] == True and 'quoted_status' in tweet:
+        quoted_status = tweet['quoted_status']
+    else:
+        quoted_status = None
+
+    if tweet['geo'] == None:
+        no_geo_count += 1
+    else:
+        no_geo_count = no_geo_count
+
+    if tweet['coordinates'] == None:
+        no_coordinates += 1
+    else:
+        no_coordinates = no_coordinates
+
+    if tweet['place'] == None:
+        no_place += 1
+    else:
+        no_place = no_place
+    # if (tweet['place'] is not None
+    #     or (tweet['geo'] is not None
+    #         or(tweet['place'] is not None))):
+
+    original_dic = {
+        '_id': tweet['id_str'],
+        'created_at': tweet['created_at'],
+        'full_text': tweet['full_text'],
+        'entities': tweet['entities'],
+        # 'source': tweet['source'],
+        'user': tweet['user'],
+        'geo': tweet['geo'],
+        'coordinates': tweet['coordinates'],
+        'place': tweet['place'],
+        'lang': tweet['lang'],
+        # 'favorite_count': tweet['favorite_count'],
+        'is_quote_status': tweet['is_quote_status'],
+        'quoted_status': quoted_status,
+    }
+
+    return original_dic, no_geo_count, no_coordinates, no_place
+
+
+if __name__ == '__main__':
+    greater_citys_id = tweepy_tokens.greater_citys_id['brisbane']
+    tweepy_api = tweepy_tokens(greater_citys_id=greater_citys_id)
+
+    original_database, scenario_database = create_couchdb()
+
+    # set date
+    start_time = datetime.date.today() - datetime.timedelta(days=7)
+    end_time = datetime.date.today()
+
+    # brisbane latitude longitude
+    geocode = "%f,%f,%fkm" % (-27.3812533, 152.713015, 40)
+
+    # search tweepy_api method
+    raw_tweets = tweepy.Cursor(tweepy_api.search, q='*', since=start_time, until=end_time,
+                               geocode=geocode, count=100, tweet_mode='extended', lang="en").items()
+    no_geo_count = 0
+    no_coordinates = 0
+    no_place = 0
+    print("Twitter harvest begin")
+    while True:
+        try:
+            original_dic, no_geo_count, no_coordinates, no_place = process_tweets(
+                raw_tweets, no_geo_count, no_coordinates, no_place)
+            original_database.save(original_dic)
+            scenario_dic = scenario_cities.execute_brisbane(
+                original_dic['_id'], original_dic['full_text'])
+
+            if scenario_dic != {}:
+                scenario_database.save(scenario_dic)
+
+        except couchdb.http.ResourceConflict:
+            pass
+        except tweepy.TweepError as TimeLimiteError:
+            print(TimeLimiteError.reason)
+            print("time out of time range")
+            time.sleep(1000)
+            continue
+        except StopIteration as FinishHarvest:
+            print("all done")
+            break
+        except Exception as AllException:
+            print(AllException)
+            time.sleep(500)
+            continue
+    print('time used: ', time.time() - start)
+    print(no_geo_count, no_coordinates, no_place)
+
